036bd1927571debbbd22963565da62f8914885ac
Pavel Sozonov <pavel.sozonov@gmail.com>
2017-06-19 00:57:26 +0300

    Unit tests for FileResource are added, in addition to integration tests

--- src/test/java/ru/inno/ps/words/resource/FileResourceTest.java
+++ src/test/java/ru/inno/ps/words/resource/FileResourceTest.java
@@ -4,7 +4,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.lang.reflect.Field;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by pavel on 18.06.17.
@@ -12,22 +17,86 @@ import static org.junit.Assert.*;
 public class FileResourceTest {
     private FileResource fileResource;
 
+    // Unit test (with reading from mocked buffered reader)
     @Test
-    public void nextWord_correctWords_assertEqualsAndTrue() throws Exception {
-         fileResource = new FileResource(
-                 "src/test/java/ru/inno/ps/words/resource/test_words_correct.txt");
-         assertEquals(fileResource.nextWord(), "Автобус");
-         assertEquals(fileResource.nextWord(), "Биатлон");
-         assertEquals(fileResource.nextWord(), "Вагон");
-         assertEquals(fileResource.nextWord(), "Город");
-         assertEquals(fileResource.nextWord(), "Депо");
-         assertTrue(fileResource.nextWord() == null);
+    public void nextWord_readFromMockedBufferedReaderWithCorrectWords_assertTrue() throws Exception {
+        BufferedReader mockedBufferedReader = mock(BufferedReader.class);
+        FileResource fileResource = new FileResource(
+                "src/test/resources/empty_file_stub.txt");
+
+        when(mockedBufferedReader.read())
+                .thenReturn((int)'А')
+                .thenReturn((int)'х')
+                .thenReturn((int)'м')
+                .thenReturn((int)'а')
+                .thenReturn((int)'т')
+                .thenReturn((int)'о')
+                .thenReturn((int)'в')
+                .thenReturn((int)'а')
+                .thenReturn((int)' ')
+                .thenReturn((int)'П')
+                .thenReturn((int)'у')
+                .thenReturn((int)'ш')
+                .thenReturn((int)'к')
+                .thenReturn((int)'и')
+                .thenReturn((int)'н')
+                .thenReturn(-1);
+
+        Class fileResourceClass = FileResource.class;
+        Field bufferedReaderField = fileResourceClass.getDeclaredField("bufferedReader");
+        bufferedReaderField.setAccessible(true);
+        bufferedReaderField.set(fileResource, mockedBufferedReader);
+        assertEquals(fileResource.nextWord(), "Ахматова");
+        assertEquals(fileResource.nextWord(), "Пушкин");
+        assertTrue(fileResource.nextWord() == null);
     }
 
+    // Unit test (with reading from mocked buffered reader)
+    @Test
+    public void nextWord_readFromMockedBufferedReaderWithLatinLetters_expectedException() throws Exception {
+        BufferedReader mockedBufferedReader = mock(BufferedReader.class);
+        FileResource fileResource = new FileResource(
+                "src/test/resources/empty_file_stub.txt");
+
+        when(mockedBufferedReader.read())
+                .thenReturn((int)' ')
+                .thenReturn((int)'W')
+                .thenReturn((int)'o')
+                .thenReturn((int)'r')
+                .thenReturn((int)'d')
+                .thenReturn(-1);
+
+        Class fileResourceClass = FileResource.class;
+        Field bufferedReaderField = fileResourceClass.getDeclaredField("bufferedReader");
+        bufferedReaderField.setAccessible(true);
+        bufferedReaderField.set(fileResource, mockedBufferedReader);
+
+        try {
+            fileResource.nextWord();
+            fail();
+        } catch (IllegalArgumentException expected) {
+            System.out.printf("Expected exception was caught. Message: %s\n", expected.getMessage());
+        }
+    }
+
+    // Integration test (with reading from the real txt file)
+    @Test
+    public void nextWord_correctWords_assertEqualsAndTrue() throws Exception {
+        fileResource = new FileResource(
+                "src/test/resources/test_words_correct.txt");
+        assertEquals(fileResource.nextWord(), "Автобус");
+        assertEquals(fileResource.nextWord(), "Биатлон");
+        assertEquals(fileResource.nextWord(), "Вагон");
+        assertEquals(fileResource.nextWord(), "Город");
+        assertEquals(fileResource.nextWord(), "Депо");
+        assertTrue(fileResource.nextWord() == null);
+    }
+
+    // Integration test (with reading from the real txt file)
     @Test
     public void nextWord_thereAreLatinLetters_expectedException() throws Exception {
         fileResource = new FileResource(
-                "src/test/java/ru/inno/ps/words/resource/test_words_with_latin_letters.txt");
+                "src/test/resources/test_words_with_latin_letters.txt");
         assertEquals(fileResource.nextWord(), "Автомат");
         assertEquals(fileResource.nextWord(), "Батон");
         assertEquals(fileResource.nextWord(), "Ваза");
@@ -38,5 +107,4 @@ public class FileResourceTest {
             System.out.printf("Expected exception was caught. Message: %s\n", expected.getMessage());
         }
     }
-
 }
