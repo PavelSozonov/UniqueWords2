08feffffbc68097907ec3503d4aab43cc36ab0eb
Pavel Sozonov <pavel.sozonov@gmail.com>
2017-06-18 23:59:09 +0300

    Text files are moved in new folder

--- src/main/java/ru/inno/ps/words/Client.java
+++ src/main/java/ru/inno/ps/words/Client.java
@@ -9,18 +9,18 @@ import ru.inno.ps.words.resource.FileResource;
 public class Client {
     public static void main(String[] args) {
         ConcurrentWordProcessor wordProcessor = new ConcurrentWordProcessor(
-                new FileResource("file1.txt"),
-                new FileResource("file2.txt"),
-                new FileResource("file3.txt"),
-                new FileResource("file4.txt"),
-                new FileResource("file5.txt"),
-                new FileResource("file6.txt"),
-                new FileResource("file7.txt"),
-                new FileResource("file8.txt"),
-                new FileResource("file9.txt"),
-                new FileResource("file10.txt"),
-                new FileResource("file11.txt"),
-                new FileResource("file12.txt"));
+                new FileResource("test_text_files/file1.txt"),
+                new FileResource("test_text_files/file2.txt"),
+                new FileResource("test_text_files/file3.txt"),
+                new FileResource("test_text_files/file4.txt"),
+                new FileResource("test_text_files/file5.txt"),
+                new FileResource("test_text_files/file6.txt"),
+                new FileResource("test_text_files/file7.txt"),
+                new FileResource("test_text_files/file8.txt"),
+                new FileResource("test_text_files/file9.txt"),
+                new FileResource("test_text_files/file10.txt"),
+                new FileResource("test_text_files/file11.txt"),
+                new FileResource("test_text_files/file12.txt"));
 
         printResult(wordProcessor.checkDuplicates());
     }
